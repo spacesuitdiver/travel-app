4e025bd4ede8670afd5db48b70fc2c7a4b91b98c
Leslie Rose <leslierose@Leslies-MacBook-Pro.local>
2018-07-24 20:17:43 -0500

    model fix

--- models/travel.js
+++ models/travel.js
@@ -2,46 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const travelSchema = new Schema({
-  location: { 
-    type: String, 
-    required: true 
-  },
-
-  date: { 
-    type: Date, 
-    default: Date.now, 
-    required: true 
-  },
-
-  dates: {
-    type: Date,
-    required: true, 
-  },
-
-  flight: 
-    { 
-      type: String, 
-      required: true 
-    },
-
-  hotel: { 
-    type: String, 
-    required: true 
-  },
-
-  days: [
-    {
-      date: new Date(),
-
-    }
-  ],
-
   user: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  }
-
+      username: '',
+      password: '',
+      loggedIn: true
+  },
+  trips: [
+      {
+          startDate: new Date(),
+          endDate: new Date(),
+          city: '',
+          state: '',
+          country: '',
+          flightNumber: '',
+          hotelNumber: '',
+          averageTemperature: 98,
+          weatherDesciptions: ['rainy', 'cloudy'],
+          days: [{
+              date: new Date(),
+              // imageURL: '',
+              pinterestObject: {}
+          }]
+      }
+  ],
 });
+
+
 
 const Travel = mongoose.model("Travel", travelSchema);
 
