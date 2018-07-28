b85c9cd9153623d8495d2ba79508e524e5229aee
laurawilsone <laura.wils204@gmail.com>
2018-07-28 12:15:48 -0500

    react header

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,46 +4,15 @@ import"./About.css";
 class About extends Component {
     render() {
         return (
-
-            <section id="services">
+            <section class="bg-primary" id="about">
                 <div class="container">
                     <div class="row">
-                    <div class="col-lg-12 text-center">
-                        <h2 class="section-heading">How it Works</h2>
-                        <hr class="my-4"></hr>
-                    </div>
-                    </div>
-                </div>
-                <div class="container">
-                    <div class="row">
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="service-box mt-5 mx-auto">
-                        <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
-                        <h3 class="mb-3">Destination</h3>
-                        <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
+                        <div class="col-lg-8 mx-auto text-center">
+                        <h2 class="section-heading text-white">We've got what you need!</h2>
+                        <hr class="light my-4"></hr>
+                        <p class="text-faded mb-4" id="subheader">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
+                        <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
                         </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="service-box mt-5 mx-auto">
-                        <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
-                        <h3 class="mb-3">Pick Your Dates</h3>
-                        <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="service-box mt-5 mx-auto">
-                        <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
-                        <h3 class="mb-3">Whats the Weather?</h3>
-                        <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 text-center">
-                        <div class="service-box mt-5 mx-auto">
-                        <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
-                        <h3 class="mb-3">Here is Your Outfit!</h3>
-                        <p class="text-muted mb-0">You have to make your websites with love these days!</p>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </section>
@@ -51,4 +20,4 @@ class About extends Component {
     }
 }
 
-export default Navbar;
+export default About;
