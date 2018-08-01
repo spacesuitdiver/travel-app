afc61b3082f0205f6409c8dddb6c408e1b2a5229
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-08-01 12:16:46 -0500

    added in User model LMR

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import HowWorks from './components/HowWorks/HowWorks';
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Weather from './components/Weather'
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Header />
-        <About />
-        <HowWorks />
-        <Contact />
-        <Weather/>
-      </div>
-    );
-  }
-}
+import inputtravel from "./pages/inputtravel/";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Navbar />
+//         <Header />
+//         <About />
+//         <HowWorks />
+//         <Contact />
+//         <Weather />
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => (
+  <Router>
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+      <HowWorks />
+      <Contact />
+      <Weather />
+      <Switch>
+        <Route exact path="/test" component={inputtravel} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
