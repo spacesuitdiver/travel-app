f5c20bd852396daef77346b69d325ececfed383e
Leslie Rose <leslierose@Leslies-MacBook-Pro.local>
2018-08-02 19:27:32 -0500

    added photo map for tumblr

--- client/src/pages/calendar/calendar.js
+++ client/src/pages/calendar/calendar.js
@@ -8,14 +8,17 @@ import { Col, Container } from "../../components/Grid";
 
 class calendar extends Component {
     state = {
-        trip: {},
-        weather: {}    
+        trip: null,
+        weather: null,
+        tumblr: null,
+        isLoading: true,
     };
 
     componentDidMount() {
         API.findOneTravel(this.props.match.params.travelId)
-        .then(res => this.setState({ trip: res.data.travel, weather: res.data.weather.weather }))
-        .catch(err => console.log(err));
+            .then(res => this.setState({ trip: res.data.travel, weather: res.data.weather, tumblr: res.data.tumblr }))
+            .then(() => this.setState({ isLoading: false }))
+            .catch(err => console.log(err));
     }
 
     // getCalendar = (event) => {
@@ -29,36 +32,54 @@ class calendar extends Component {
     //   };
 
     // viewTripDetails = clickedEvent => {
-	// 	// console.log("event clicked! - before formatting:", clickedEvent);
-	// 	API.findOneTravel(this.props.match.params.id).then(response => {
-	// 		const selectedTrip = { ...response.data, }
-	// 		selectedTrip.start = moment(selectedTrip.start).format("dddd, MMMM, D, YYYY,  h:mm A"); 
-	// 		selectedTrip.end = moment(selectedTrip.end).format("dddd, MMMM, D, YYYY,  h:mm A");
-	// 		// selectedSession.newStudyBuddyInfo = { email:"", studyBuddyEmailMsg: ""};
-	// 		// console.log("selectedSession after formatting:", selectedSession);
+    // 	// console.log("event clicked! - before formatting:", clickedEvent);
+    // 	API.findOneTravel(this.props.match.params.id).then(response => {
+    // 		const selectedTrip = { ...response.data, }
+    // 		selectedTrip.start = moment(selectedTrip.start).format("dddd, MMMM, D, YYYY,  h:mm A"); 
+    // 		selectedTrip.end = moment(selectedTrip.end).format("dddd, MMMM, D, YYYY,  h:mm A");
+    // 		// selectedSession.newStudyBuddyInfo = { email:"", studyBuddyEmailMsg: ""};
+    // 		// console.log("selectedSession after formatting:", selectedSession);
 
-	// 		// this.setState({
-	// 		// 	selectedSession: selectedSession,
-	// 		// 	showSessionDetailModal: true
-	// 		// })
-	// 	})
-	// };
+    // 		// this.setState({
+    // 		// 	selectedSession: selectedSession,
+    // 		// 	showSessionDetailModal: true
+    // 		// })
+    // 	})
+    // };
 
     render() {
 
         return (
             <Container>
-               
+                {!this.state.isLoading &&
+                    <div>
                         <h3><strong>Your trip details</strong></h3>
-                        City: {this.state.trip.city}<br/>
-                        Country: {this.state.trip.country}<br/>
-                        Start Date: {this.state.trip.startDate}<br/>
-                        End Date: {this.state.trip.endDate}<br/>
-                        {/* <strong>Weather</strong><br/>
-                        Forecast: {this.state.weather.weather.coord.lon}<br/>
-                        Temp (Celcius): {this.state.weather.weather}<br/> */}
+                        City: {this.state.trip.city}<br />
+                        Country: {this.state.trip.country}<br />
+                        Start Date: {this.state.trip.startDate}<br />
+                        End Date: {this.state.trip.endDate}<br />
+                        <h3><strong>Weather details</strong></h3>
+                        <p>{this.state.weather.weather[0].description}</p>
+                        <h3><strong>Temperature (celius)</strong></h3>
+                        <p>{this.state.weather.main.temp}</p>
+                        <h3>Your fashion pics</h3>
+                        
 
+                        {this.state.tumblr.length ? (
 
+                            <List>
+                                {this.state.tumblr.map(tum => (
+                                        <ListItem key={tum._id}>
+                                         <img src={tum.photos[0].original_size.url}/>
+                                        </ListItem>
+                                    ))}
+                            </List>
+                        ) : (
+                                <h3>No Results to Display</h3>
+                            )}
+
+                    </div>
+                }
             </Container>
 
         );
@@ -66,3 +87,4 @@ class calendar extends Component {
 }
 
 export default calendar;
+
