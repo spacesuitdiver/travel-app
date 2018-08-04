9e771bfa27d3074889f4465e802ad34e16e46fee
Leslie Rose <leslierose@Leslies-MBP.attlocal.net>
2018-08-03 19:58:00 -0500

    calendar update

--- client/src/pages/CalendarTest/CalendarTest.js
+++ client/src/pages/CalendarTest/CalendarTest.js
@@ -1,29 +1,103 @@
-import React from "react";
-import Calendar from "../../components/Calendar";
+import React from 'react'
+import { render } from 'react-dom'
+import Dropdown from 'react-bootstrap/lib/Dropdown'
+import MenuItem from 'react-bootstrap/lib/MenuItem'
 
-class CalendarApp extends React.Component {
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import Selectable from '../../components/Calendar'
 
-  state = {
-    currentMonth: new Date(),
-    selectedDate: new Date()
-};
+
+// let demoRoot =
+//   'https://github.com/intljusticemission/react-big-calendar/tree/master/examples/demos'
+
+const EXAMPLES = {
+  selectable: 'Create events',
+}
+
+class Example extends React.Component {
+  constructor(...args) {
+    super(...args)
+
+    const hash = (window.location.hash || '').slice(1)
+
+    this.state = {
+      selected: EXAMPLES[hash] ? hash : 'basic',
+    }
+  }
+
+  select = selected => {
+    this.setState({ selected })
+  }
   render() {
+    let selected = this.state.selected
+    let Current = {
+      selectable: Selectable,
+    }[selected]
+
     return (
-      <div className="App">
-        <header>
-          <div id="logo">
-            <span className="icon">date_range</span>
-            {/* <span>
-              react<b>calendar</b>
-            </span> */}
+      <div className="app">
+        <div className="jumbotron">
+          <div className="container">
+            <h1>
+              Big Calendar <i className="fa fa-calendar" />
+            </h1>
+            <p>such enterprise, very business.</p>
+            <p>
+              <a href="#intro">
+                <i className="fa fa-play" /> Getting started
+              </a>
+              {' | '}
+              <a href="#api">
+                <i className="fa fa-book" /> API documentation
+              </a>
+              {' | '}
+              <a
+                target="_blank"
+                href="https://github.com/intljusticemission/react-big-calendar"
+              >
+                <i className="fa fa-github" /> github
+              </a>
+            </p>
           </div>
-        </header>
-        <main>
-          <Calendar />
-        </main>
+        </div>
+        <Selectable/>
+
+        <div className="examples">
+          <header className="examples--header">
+            <div className="examples--view-source">
+              {/* <a target="_blank" href={demoRoot + '/' + selected + '.js'}>
+                <strong>
+                  <i className="fa fa-code" />
+                  {' View example source code'}
+                </strong>
+              </a> */}
+            </div>
+            <Dropdown className="examples--dropdown" pullRight>
+              <Dropdown.Toggle bsStyle="link" className="dropdown--toggle ">
+                {EXAMPLES[selected]}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                {Object.entries(EXAMPLES).map(([key, title]) => (
+                  <MenuItem href={`#${key}`} onClick={() => this.select(key)}>
+                    {title}
+                  </MenuItem>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </header>
+        </div>
+        {/* <div className="docs"> */}
+          {/* <div className="contain section">
+            <Intro />
+          </div>
+          <Api className="contain section" />
+        </div> */}
       </div>
-    );
+    )
   }
 }
 
-export default CalendarApp;
+document.addEventListener('DOMContentLoaded', () => {
+  render(<Example />, document.getElementById('root'))
+})
