d0355f370a34a45adfd53e066829353b51925952
Andrea <andreazepeda003@gmail.com>
2018-08-06 16:50:28 -0500

    front-end

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -58,6 +58,7 @@ class Header extends Component {
             })
         }
         
+
             
     
       onLogin() { //
--- client/src/components/HowWorks/HowWorks.js
+++ client/src/components/HowWorks/HowWorks.js
@@ -17,7 +17,7 @@ const HowWorks = () => (
     <div class="col-lg-3 col-md-6 text-center">
       <div class="service-box mt-5 mx-auto">
         {/* <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i> */}
-       
+        <img class="howWorks" src="/images/where.jpg" alt="Destination"/>
         <h3 class="mb-3">Destination</h3>
         <p class="text-muted mb-0">Where are you going?</p>
       </div>
@@ -25,6 +25,7 @@ const HowWorks = () => (
     <div class="col-lg-3 col-md-6 text-center">
       <div class="service-box mt-5 mx-auto">
         {/* <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i> */}
+        <img class="howWorks" src="/images/dates.jpg" alt="Dates"/>
         <h3 class="mb-3">Dates</h3>
         <p class="text-muted mb-0">Let us know when your next trip is!</p>
       </div>
@@ -32,6 +33,7 @@ const HowWorks = () => (
     <div class="col-lg-3 col-md-6 text-center">
       <div class="service-box mt-5 mx-auto">
         {/* <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i> */}
+        <img class="howWorks" src="/images/whether.jpg" alt="Weather"/>
         <h3 class="mb-3">What's the Weather?</h3>
         <p class="text-muted mb-0">We will take your date range to find out the weather in your destination!</p>
       </div>
@@ -39,6 +41,7 @@ const HowWorks = () => (
     <div class="col-lg-3 col-md-6 text-center">
       <div class="service-box mt-5 mx-auto">
         {/* <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i> */}
+        <img class="howWorks" src= "/images/pack.jpg" alt="Outfit"/>
         <h3 class="mb-3">Here's Your Outfit!</h3>
         <p class="text-muted mb-0">We will give you some inspiration to help you pack for you trip!</p>
       </div>
